1a742ec616e9e6ea21f300735eaf1f3f251d5d75
Issa-Abbani <issaabbani04@gmail.com>
2025-07-25 17:02:03 +0300

    Handled the DOM aspect of the delete tab

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const myLibrary = [];
+const body = document.querySelector('body');
 const addBookBtn = document.querySelector('#add-book-btn');
 const addBookTab = document.querySelector('aside');
 const closeBookTab = document.querySelector('.exit-form');
 const addBookInput = document.querySelectorAll('.form-elem > input');
+const deleteBtns = document.querySelectorAll('del-btn-icon');
+const exitDelBtn = document.querySelector('.exit-del');
 
 
 //save in JSON form
@@ -30,6 +33,7 @@ function toggleAddTab(){
   addBookBtn.addEventListener("click", ()=>{
     if(!(addBookTab.classList.contains('active-add'))){
       addBookTab.classList.add('active-add');
+      body.classList.add('background-locked');
       addBookInput.forEach(input => input.value = '');
     }
   })
@@ -37,14 +41,40 @@ function toggleAddTab(){
   closeBookTab.addEventListener("click", ()=>{
     if (addBookTab.classList.contains('active-add')) {
       addBookTab.classList.remove('active-add');
+      body.classList.remove('background-locked');
       addBookInput.forEach(input => input.value = '');
     }
   })
 }
 
-// function toggleDeleteTab(){
+//Delete button setting
+function toggleDeleteTab() {
+  const tableBody = document.querySelector('main section table tbody');
+  const delTab = document.querySelector('.confirm-deletion');
 
-// }
+  exitDelBtn.addEventListener("click", ()=>{
+      if(delTab.classList.contains('active-del')){
+        delTab.classList.remove('active-del');
+        body.classList.remove('background-locked');
+      }
+  })
+
+  tableBody.addEventListener('click', (e) => {
+    const clicked = e.target;
+
+    if (clicked && clicked.id === 'del-btn-icon') {
+      // OPEN your tab or handle whatever logic you want here
+      console.log('Delete button clicked:', clicked.closest('tr'));
+      
+
+      if(!(delTab.classList.contains('active-del'))){
+        delTab.classList.add('active-del');
+        body.classList.add('background-locked');
+      }
+
+    }
+  });
+}
 
 //toggling the add book button in the add book tab
 function addBookButton(){
@@ -114,7 +144,7 @@ function renderLibrary(){
         <td>${book.author}</td>
         <td>${book.pages}</td>
         <td>${book.read_stat}</td>
-        <td class="remove-btn"><img src="Assets/trash.svg" loading="lazy" alt="remove" /></td>
+        <td class="remove-btn"><img src="Assets/trash.svg" id="del-btn-icon" loading="lazy" alt="remove" /></td>
       `;
 
       tbody.appendChild(tr);
@@ -134,8 +164,8 @@ loadLibrary();
 
 renderLibrary();
 
-
 toggleAddTab();
 
-
 addBookButton();
+
+toggleDeleteTab();
